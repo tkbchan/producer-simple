fa0799c67521943dfcd7a7e0db42bfabc6684dc5
Timothy Kyle Chan <87596613+tkbchan@users.noreply.github.com>
2021-08-03 13:27:39 +0800

    Update AppConfigs.java

--- src/main/java/org/timothy/producer/common/AppConfigs.java
+++ src/main/java/org/timothy/producer/common/AppConfigs.java
@@ -8,8 +8,8 @@ public class AppConfigs {
     public final static int numEvents = 125;
     public static int MAX_NO_MESSAGE_FOUND_COUNT = 100;
     // for TwtProducer
-    public final static String consumerKey = "Pa9M8nj3tl1iNbFhIjFJV8C6B";
-    public final static String consumerSecret = "nxPxxkO21Fp7XEwESftMS9So6V8h9wXzRWKQpr8KnbXIm4Rfth";
-    public final static String token = "702679736393166848-6LDdX6qeDpmFraAw3Yfi8OxkCxE4gNr";
-    public final static String secret = "TwAtaegjrVsRlF77sNZy1utJSjznc8O6MPGeCoIg9ZCVi";
+    public final static String consumerKey = "API_KEY";
+    public final static String consumerSecret = "API_KEY";
+    public final static String token = "API_KEY";
+    public final static String secret = "API_KEY";
 }
