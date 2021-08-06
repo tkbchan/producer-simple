4774807b9794c29fe759d91665efc4763b237aad
Timothy Kyle Chan <tkbchan24@gmail.com>
2021-08-06 15:55:39 +0800

    Added Try Catch

--- src/main/java/org/timothy/producer/Consumer.java
+++ src/main/java/org/timothy/producer/Consumer.java
@@ -2,6 +2,7 @@ package org.timothy.producer;
 
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.timothy.producer.common.AppConfigs;
@@ -18,28 +19,35 @@ public class Consumer{
 
         consumer.subscribe(Collections.singletonList(AppConfigs.topicName));
         int noMessageFound = 0;
+        try{
+            while(true){
+                ConsumerRecords<Integer,String> records = consumer.poll(Duration.ofMillis(1000));
 
-        while(true){
-            ConsumerRecords<Integer,String> records = consumer.poll(Duration.ofMillis(1000));
-            
-            if (records.count() == 0) {
-                noMessageFound++;
-                if (noMessageFound > AppConfigs.MAX_NO_MESSAGE_FOUND_COUNT)
-                    break;
-                else
-                    continue;
+                if (records.count() == 0) {
+                    noMessageFound++;
+                    if (noMessageFound > AppConfigs.MAX_NO_MESSAGE_FOUND_COUNT)
+                        break;
+                    else
+                        continue;
+                }
+
+                records.forEach(record -> logger.info("Received new record: " +
+                        " Key: " + record.key() +
+                        ", Value: " + record.value() +
+                        ", Topic: " + record.topic() +
+                        ", Partition: " + record.partition() +
+                        ", Offset: " + record.offset() + "\n"
+                ));
+                consumer.commitSync();
+
             }
-
-            records.forEach(record -> logger.info("Received new record: " +
-                    " Key: " + record.key() +
-                    ", Value: " + record.value() +
-                    ", Topic: " + record.topic() +
-                    ", Partition: " + record.partition() +
-                    ", Offset: " + record.offset() + "\n"
-            ));
-            consumer.commitAsync();
-
+        } catch (WakeupException e) {
+            // Ignore exception if closing
+            throw e;
+        } catch (RuntimeException re) {
+            logger.error("RuntimeException {}", re);
+        } finally {
+            consumer.close();
         }
-        consumer.close();
     }
 }
