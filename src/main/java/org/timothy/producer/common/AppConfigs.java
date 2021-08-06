aed7d3caee3666db5766d4eac3e0b4d6c87b9015
Timothy Kyle Chan <tkbchan24@gmail.com>
2021-08-06 15:08:08 +0800

    Minor Modifications

--- src/main/java/org/timothy/producer/common/AppConfigs.java
+++ src/main/java/org/timothy/producer/common/AppConfigs.java
@@ -4,7 +4,7 @@ public class AppConfigs {
     public final static String applicationID = "Producer";
     public final static String consumerGroupID = "java-group-consumer";
     public final static String bootstrapServers = "localhost:9092,localhost:9093";
-    public final static String topicName = "twitter-testing";
+    public final static String topicName = "topic-test";
     public final static int numEvents = 5000;
     public static int MAX_NO_MESSAGE_FOUND_COUNT = 100;
     // for TwtProducer
